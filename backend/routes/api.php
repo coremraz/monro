55681d74a770eeb0ab7433586e08c3a35f6aefcb
coremraz <50779187+coremraz@users.noreply.github.com>
2024-09-01 20:16:38 +0300

    done

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\v1\RegisteredUserController;
+use App\Http\Controllers\Api\v1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1')->middleware('cors')->group(function () {
+    //условный CRUD пользователя
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::get('/user/{id}', [UserController::class, 'show']);
+    Route::get('/users/all', [UserController::class, 'index']);
+    Route::patch('/user/{id}', [UserController::class, 'update']);
+
+    //Отправка и верификация кода
+    Route::post('/user/sendCode', [UserController::class, 'requestConfirmationCode']);
+    Route::post('/user/verifyCode', [UserController::class, 'checkConfirmationCode']);
 });
